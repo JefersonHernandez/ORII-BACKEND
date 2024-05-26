a29f97f8c07b3a52f27053f565ac7d22b46f358a
Jeferson Hernandez <asimplemailmore@gmail.com>
2024-05-25 22:51:13 -0500

    feat: update db connection

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,23 +1,21 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { User } from "./entity/User";
-import { Facultad } from "./entity/Facultad";
-import { Programa } from "./entity/Programa";
 import { Actor } from "./entity/Actor";
+import { Facultad } from "./entity/Facultad";
 import { MovilidadActor } from "./entity/MovilidadActor";
-
-
+import { Programa } from "./entity/Programa";
+import { User } from "./entity/User";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: "host.docker.internal",
+  host: "ori-db.cpi15s7eozdu.us-east-1.rds.amazonaws.com",
   port: 3306,
-  username: "root",
-  password: "",
+  username: "admin",
+  password: "o2BKuYhDKbekUnbMX7eu",
   database: "ori-db",
   synchronize: false,
   logging: false,
-  entities: [User,Facultad,Programa,Actor,MovilidadActor],
+  entities: [User, Facultad, Programa, Actor, MovilidadActor],
   migrations: [],
   subscribers: [],
 });
